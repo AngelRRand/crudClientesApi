1860b8ae239d460e260b611f8a8a28a68b955773
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-13 16:58:38 -0300

    templet

---
+++ src/View/Inicio.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Inicio = () => {
+
+
+  return (
+    <View>
+      <Text>Inicio</Text>
+    </View>
+  )
+
+  
+}
+
+export default Inicio
