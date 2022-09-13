d83467cfbf501b2fd903d572e818ea14b38afa00
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-13 17:03:35 -0300

    Screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Inicio from './src/View/Inicio';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -9,7 +10,15 @@ export default function App() {
   return (
     <>
       <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName='Inicio'
+        >
+          <Stack.Screen
+            name='inicio'
+            component={Inicio}
+          />
 
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
