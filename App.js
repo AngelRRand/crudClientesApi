b571803246b1ad3d06c2052c9a9d18c3e6d9a568
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-13 17:22:46 -0300

    FixedDependeciesPaperInstall

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
+//React-Native-Navegation
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//React-Native-Paper
+
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import DetallesCliente from './src/View/DetallesCliente';
 import Inicio from './src/View/Inicio';
 import NuevoCliente from './src/View/NuevoCliente';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
+
+
+  
+
 
 
   return (
