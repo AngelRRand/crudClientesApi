741d8cae24eae59bae942d261820d88588ff025f
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-13 21:00:32 -0300

    refrescarApi

--- src/View/Inicio.js
+++ src/View/Inicio.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { View, Text, FlatList } from 'react-native'
-import { List, Headline } from 'react-native-paper';
+import { List, Headline , Button} from 'react-native-paper';
 import globalStyles from '../Styles/StylesGlobal';
+import { Ionicons } from '@expo/vector-icons';
 import axios from 'axios';
 
 const Inicio = () => {
 
   const [clientes, setClientes] = useState([]);
+  const [consultarAPI, setConsultarAPI] = useState(true);
 
   useEffect(() => {
     const cargarClientes = async () => {
@@ -17,13 +19,17 @@ const Inicio = () => {
         console.log((error))
       }
     }
-    cargarClientes()
-  }, []);
+    if(consultarAPI){
+      cargarClientes()
+    }
+  }, [consultarAPI]);
 
   return (
     <View>
       <Headline style={globalStyles.titulo}>{clientes.length === 0 ? "Aun no hay clientes" : "Clientes"}</Headline>
-      
+      <Button>
+        Nuevo cliente
+      </Button>
       <FlatList
         data={clientes}
         keyExtractor={client => (client.id).toString()}
